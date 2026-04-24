781b1f98f83807b1ab9c50ba545439169f695867
Andrew Kodkod <678665+akodkod@users.noreply.github.com>
2026-04-24 15:08:15 +0100

    Bump version

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import prohibitedComments from "./rules/prohibited-comments";
 const plugin = {
   meta: {
     name: "eslint-plugin-sane",
-    version: "2.0.0",
+    version: "2.0.1",
   },
   rules: {
     "attribute-formatting": attributeFormatting,
